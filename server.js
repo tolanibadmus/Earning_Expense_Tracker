de10a561324fce89439f039e9310307475a61586
Kaosarat Badmus <badmustolani10@gmail.com>
2025-01-06 13:58:52 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+app.use(express.json());
+
+
+
+app.listen(PORT);
